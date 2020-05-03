a0f579ffaa0bf9f6e05af124d59f4268956e9ec2
Yuri dos Santos <YuriSantos@SETTAcos-iMac.local>
2020-05-03 20:46:32 -0300

    Implementar cadastro via banco

--- db.js
+++ db.js
@@ -3,45 +3,44 @@ const db = new sqlite3.Database('./ws.db')
 
 db.serialize(function() {
 
-    //Criar a tabela
-    // db.run(`
-    //    CREATE TABLE IF NOT EXISTS ideas(
-    //        id INTEGER PRIMARY KEY AUTOINCREMENT,
-    //        image TEXT,
-    //        title TEXT,
-    //        category TEXT,
-    //        description TEXT,
-    //        link TEXT
-    //    );
-   // `)
+    // Criar a tabela
+    db.run(`
+        CREATE TABLE IF NOT EXISTS ideas(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            image TEXT,
+            title TEXT,
+            category TEXT,
+            description TEXT,
+            link TEXT
+        );
+    `)
 
-    //Inserir dados na tabela
-    // const query = `
-   // INSERT INTO ideas(
-   //     image,
-   //     title,
-   //     category,
-   //     description,
-   //     link
-   // ) VALUES (?,?,?,?,?);
-//`
+   // Inserir dado na Tabela
+//    const query = `
+//    INSERT INTO ideas(
+//        image,
+//        title,
+//        category,
+//        description,
+//        link
+//    ) VALUES (?,?,?,?,?);
+//    `
+//    const values = [
+//        "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+//        "Cursos de Programação",
+//        "Estudo",
+//        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa voluptas minus aliquid quibusdam dolorum suscipit animi facilis eaque, reprehenderit modi necessitatibus repellat omnis magni ex veritatis eius velit. Nobis, quae?",
+//        "https://rocketseat.com.br/",
+//    ]
 
-   // const values = [
-   // "https://image.flaticon.com/icons/svg/2729/2729007.svg",
-   // "Cursos de Programação",
-   // "Estudo",
-   // "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa voluptas minus aliquid quibusdam dolorum suscipit animi facilis eaque, reprehenderit modi necessitatibus repellat omnis magni ex veritatis eius velit. Nobis, quae?",
-   // "https://rocketseat.com.br/",
-//]
+//    db.run(query, values, function(err) {
+//        if (err) return console.log(err)
 
-   // db.run(query, values, function(err) {
-   //     if (err) return console.log(err)
-
-   //     console.log(this)
-   // })
+//        console.log(this)
+//    })
 
 
-   //Deletar um dado da tabela
+   // Deletar um dado da tabela
    // db.run(`DELETE FROM ideas WHERE id = ?`, [1], function(err) {
    //     if (err) return console.log(err)
 
@@ -58,3 +57,4 @@ db.serialize(function() {
 
 })
 
+module.exports = db
